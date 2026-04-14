e0ca9d47b92ff7a532c35beace74edb9f645cd49
TheDarkness6859 <suarezgarcia939@gmail.com>
2026-04-13 21:32:18 -0500

    refactor(UserController): delete Scanner dependency

--- src/main/java/com/management/jupiter/controllers/UserController.java
+++ src/main/java/com/management/jupiter/controllers/UserController.java
@@ -2,15 +2,9 @@ package com.management.jupiter.controllers;
 
 import com.management.jupiter.models.User;
 import com.management.jupiter.services.UserServices;
-import com.management.jupiter.util.scanner.ScannerUtil;
 
 public class UserController {
 
-    private final ScannerUtil input;
-
-    public UserController (ScannerUtil input) {
-        this.input = input;
-    }
 
     public User LoginController(String email, String password) throws Exception {
 
