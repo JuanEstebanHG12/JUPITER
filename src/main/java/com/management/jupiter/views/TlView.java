7bef556b7d44fbbfc51849f25799232fa8d435cd
TheDarkness6859 <suarezgarcia939@gmail.com>
2026-04-13 20:18:34 -0500

    refactor(TlView): restructure to delegate logic to controller

---
+++ src/main/java/com/management/jupiter/views/TlView.java
@@ -0,0 +1,74 @@
+package com.management.jupiter.views;
+import com.management.jupiter.models.Tl;
+import com.management.jupiter.ui.users.TeamLeaderUI;
+import com.management.jupiter.util.scanner.ScannerUtil;
+
+public class TlView {
+
+    private final ScannerUtil input;
+    private final Tl controller;
+
+    public TlView(ScannerUtil input, Tl controller){
+        this.input = input;
+        this.controller = controller;
+    }
+
+    public void show (Tl tl){
+
+        int dec;
+
+        do{
+
+            TeamLeaderUI.teamLeader(tl);
+            TeamLeaderUI.tlDec();
+            dec = input.readInt("Which is your decision");
+
+            switch (dec){
+                case 1:
+                    //Add Soon...
+                    break;
+                case 2:
+                    //Add Soon...
+                    break;
+                case 3:
+                    //Add Soon...
+                    break;
+                case 4:
+                    coderManagement(tl);
+                    break;
+            }
+
+        }while (dec != 0);
+
+    }
+
+    public void coderManagement(Tl tl){
+
+        int dec;
+
+        do{
+
+            TeamLeaderUI.teamLeader(tl);
+            TeamLeaderUI.tlCoder();
+            dec = input.readInt("Which is your decision");
+
+            switch (dec){
+                case 1:
+                    //Add Soon...
+                    break;
+                case 2:
+                    //Add Soon...
+                    break;
+                case 3:
+                    //Add Soon...
+                    break;
+                case 4:
+                    //Add Soon...
+                    break;
+            }
+
+        }while (dec != 0);
+
+    }
+
+}
