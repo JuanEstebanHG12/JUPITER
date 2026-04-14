ffa4fd06303e08bcbd58863b29c6676004ac6cd7
TheDarkness6859 <suarezgarcia939@gmail.com>
2026-04-13 20:22:33 -0500

    feat(LoginUI): add UI login header to users

---
+++ src/main/java/com/management/jupiter/ui/auth/LoginUI.java
@@ -0,0 +1,17 @@
+package com.management.jupiter.ui.auth;
+
+public class LoginUI {
+
+    public static void login(){
+
+        String loginHeader = """
+                -----------------------------
+                |          LOGIN            |
+                -----------------------------
+                """;
+
+        System.out.println(loginHeader);
+
+    }
+
+}
